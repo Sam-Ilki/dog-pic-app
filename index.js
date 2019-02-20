2a323e4a3ea436d98bcaff348c58204560ed9996
Julie Broderick <broderick.julie@gmail.com>
2019-02-19 23:22:55 -0600

    exercise 3 done, update ex 1 & 2 function names

--- index.js
+++ index.js
@@ -8,28 +8,64 @@ function createDogHTML(responseJson) {
     let dogs = responseJson.message;
     let images = dogs.map(dog => `<img src=${dog}>\n`);
     let dogString = images.join('');
-    render(dogString);
+    renderRandom(dogString);
     console.log("dogs", dogs)
     console.log("images", images);
     console.log(typeof dogString);
 }
 
-function onSubmit(){
-    $('form').on('submit', function(event){
+function onSubmitQuantity(){
+    $('.quantity').on('submit', function(event){
         event.preventDefault();
-        clearDogs();
+        clearDogs('.displayDog');
         let number = $('#quantity').val();
         console.log(number);
         getDogInfo(number);
     })
 }
 
-function clearDogs() {
-    $('.displayDog').empty();
+function clearDogs(input) {
+    $(input).empty();
 }
 
-function render(string){
+function renderRandom(string){
     $('.displayDog').append(string);
 }
 
-$(onSubmit());
+function getInfoBreed(breed) {
+    fetch(`https://dog.ceo/api/breed/${breed}/images/random`)
+        .then(response => response.json())
+        .then(responseJson => getBreedHTML(responseJson));
+}
+
+function getBreedHTML(responseJson) {
+    console.log(responseJson);
+    let html;
+    if (responseJson.status === "success") {
+        html = `<img src="${responseJson.message}">`;  
+    } else {
+        html = `<p>Sorry, ${responseJson.message} 🐶</p>`
+    }
+    
+    renderBreed(html); 
+}
+
+function onSubmitBreed() {
+    $('.breed').on('submit', function(event) {
+        event.preventDefault();
+        clearDogs('.displayBreed');
+        let breed = $('#breed').val();
+        getInfoBreed(breed);
+    })
+}
+
+function renderBreed(string) {
+    $('.displayBreed').append(string);
+}
+
+
+
+$(() => {
+    onSubmitQuantity();
+    onSubmitBreed();
+});
